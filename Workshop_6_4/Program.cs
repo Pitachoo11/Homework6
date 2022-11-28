bf4c78408f3f50048c2d4b7933c3400d7e82fe36
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-28 22:36:25 +0300

    Workshop_6_4

---
+++ Workshop_6_4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+void FillArray(int[] array)
+{
+    var rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(1,10);
+    }
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] copyOfArray = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+        copyOfArray[i] = array[i];
+
+    return copyOfArray;
+}
+
+int[] a = new int[10];
+FillArray(a);
+System.Console.WriteLine(String.Join(", ", CopyArray(a)));
