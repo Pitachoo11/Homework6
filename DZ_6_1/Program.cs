3afe784887800c72e5d588fd586bc4ed1ba7a404
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-29 12:08:19 +0300

    DZ_6_1

---
+++ DZ_6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System.Linq;
+
+void CountPositiveNumbers(string data)
+{
+   int[] InputArray = Array.ConvertAll(data.Replace(" ", "").Split(","), int.Parse);
+   int count = InputArray.Count(x => x > 0);
+   Console.WriteLine($"Кол-во элементов > 0: {count}");
+}
+
+Console.WriteLine("Введите произвольное количество целых чисел, разделенных запятой:");
+string data = Console.ReadLine();
+
+CountPositiveNumbers(data);
